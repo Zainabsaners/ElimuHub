ecf5ab968eb7c14df9afbf5f0635e3a345eaaad4
Zainabsaners <zainabsaners@gmail.com>
2026-06-16 13:47:05 +0300

    updated json foe build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(), 
-    tailwindcss()],
 
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: (import.meta.env.VITE_API_URL) || 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  }
 })
